0707c243a41f906b0dd86cabcf26e77e5b3a231c
LiceoScientificoLeonardo <virtualvisit360@liceoscientificoleonardo.it>
2025-06-03 11:07:53 +0200

    Delete Static/js directory

--- Static/js/Rit.js
+++
@@ -1,9 +0,0 @@
-function rit() {
-    window.location.href = "../page/Visita.html";
-}
-function rit2() {
-    window.location.href = "../page/Visita2.html";
-}
-function rit3() {
-    window.location.href = "../page/Visita3.html";
-}
